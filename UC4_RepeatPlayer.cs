8dc166b4cf04e4b151cf1a53a30967fd317ea9a8
RISHABH <gguptarishabh82@gmail.com>
2022-03-02 17:15:05 +0530

    Repeat Player

---
+++ UC4_RepeatPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem_Day4
+{
+    internal class UC4_RepeatPlayer
+    {
+        public static void RepeatPlayer()
+        {
+            int start = 0;
+            int final = 100;
+            int n;
+            Random random = new Random();
+            n = random.Next(95, 101);
+            if (n == final)
+            {
+                Console.WriteLine("Starts from Starting Position");
+            }
+            else
+            {
+                Console.WriteLine("Play Continously");
+            }
+        }
+    }
+}
