5712d0c256d41271993fd271ca4620aec0ab6c05
RISHABH <gguptarishabh82@gmail.com>
2022-03-02 17:11:41 +0530

    Rooling Dice

---
+++ UC2_RollingDice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem_Day4
+{
+    internal class UC2_RollingDice
+    {
+        public static void RollingDice()
+        {
+            int dice;
+            Random random = new Random();
+            dice = random.Next(1, 7);
+            Console.WriteLine("No. of shown in Dice is : " + dice);
+        }
+    }
+}
